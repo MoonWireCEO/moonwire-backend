4b4e12cbdfa9f444cf5e4914dffb706b1016fcdb
MoonWireCEO <andrew@moonwire.app>
2025-05-05 10:35:07 -0400

    Update cache.py

--- src/cache.py
+++ src/cache.py
@@ -1,19 +1,12 @@
-# src/cache.py
-
 class SignalCache:
     def __init__(self):
-        # Use a simple Python dictionary instead of Redis for now
-        self.cache = {}
-
-    def set_signal(self, key, value):
-        # Set a value in the in-memory cache
-        self.cache[key] = value
+        self._store = {}
 
     def get_signal(self, key):
-        # Retrieve a value from the cache
-        return self.cache.get(key, None)
+        return self._store.get(key, [])
 
-    def delete_signal(self, key):
-        # Delete a value from the cache
-        if key in self.cache:
-            del self.cache[key]
+    def set_signal(self, key, value):
+        self._store[key] = value
+
+    def clear(self):
+        self._store.clear()
