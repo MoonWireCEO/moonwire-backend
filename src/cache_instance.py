023244932f8230bbb069ff8a84087b4d833e8ba2
MoonWireCEO <andrew@moonwire.app>
2025-05-05 10:15:50 -0400

    Update cache_instance.py

--- src/cache_instance.py
+++ src/cache_instance.py
@@ -1,3 +1,17 @@
 from src.cache import SignalCache
 
 cache = SignalCache()
+
+class SignalCache:
+    def __init__(self):
+        self.data = {}
+
+    def store_signal(self, asset, signal):
+        key = f"{asset}_signals"
+        if key not in self.data:
+            self.data[key] = []
+        self.data[key].append(signal)
+
+    def get_signals(self, asset):
+        key = f"{asset}_signals"
+        return self.data.get(key, [])
