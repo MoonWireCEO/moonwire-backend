006d033df9871f883c1e1bb592eb398bdc7e9eb0
MoonWireCEO <andrew@moonwire.app>
2025-05-05 10:35:27 -0400

    Update cache_instance.py

--- src/cache_instance.py
+++ src/cache_instance.py
@@ -1,22 +1,3 @@
-class SignalCache:
-    def __init__(self):
-        self.signals = {}
-        self.sentiment_scores = {}
+from src.cache import SignalCache
 
-    def get_signals(self, key):
-        return self.signals.get(key, [])
-
-    def set_signal(self, key, signals):
-        self.signals[key] = signals
-
-    def store_signal(self, asset, signal):
-        key = f"{asset}_signals"
-        if key not in self.signals:
-            self.signals[key] = []
-        self.signals[key].append(signal)
-
-    def get_sentiment(self, asset):
-        return self.sentiment_scores.get(asset, 0.0)
-
-    def set_sentiment(self, asset, score):
-        self.sentiment_scores[asset] = score
+cache = SignalCache()
